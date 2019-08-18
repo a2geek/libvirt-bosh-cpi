33273405918b33810f7b7b0d763b3cb395d237c8
Rob Greene <robgreene@gmail.com>
2019-08-18 12:07:29 -0500

    Excluding 0xfe for MAC address as Libvirt uses it for a specific type of network interface.

--- src/util/mac.go
+++ src/util/mac.go
@@ -8,13 +8,20 @@ import (
 // GenerateRandomHardwareAddr per https://stackoverflow.com/questions/21018729/generate-mac-address-in-go
 func GenerateRandomHardwareAddr() (net.HardwareAddr, error) {
 	buf := make([]byte, 6)
-	_, err := rand.Read(buf)
-	if err != nil {
-		return nil, err
-	}
+	for {
+		_, err := rand.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+	
+		// Set local bit, ensure unicast address
+		buf[0] = (buf[0] | 2) & 0xfe
 
-	// Set local bit, ensure unicast address
-	buf[0] = (buf[0] | 2) & 0xfe
+		// Libvirt apparently uses 0xfe for tap devices; only keeping MAC addresses without 0xfe
+		if buf[0] != 0xfe {
+			break;
+		}
+	}
 
 	return net.HardwareAddr(buf), nil
 }
